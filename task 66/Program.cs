ef6a32aa5b1e3cf8a85b9420a2c275a8a1241634
danyasman <123205133+danyasman@users.noreply.github.com>
2023-03-07 23:38:40 +0600

    add
    add

---
+++ task 66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Recursion(string sum)
+{
+    Console.Write(sum);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return n + CountNaturalSum(m, n - 1);
+}
+int m = Recursion("Введите M: ");
+int n = Recursion("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
